29f9987fe83e6ea7eb1bf6a8300937251d6ef248
Walaa Mostafa Abd_Elhafeez <mostafawalaa777@gmail.com>
2025-12-29 14:12:44 +0200

    Add files via upload
    StudentInfo is a simple C# Windows Forms project that allows users to manage student information.
    It includes features like:
    
    Adding, and deleting student records
    
    Displaying student details in a user-friendly interface
    
    Practicing basic C# concepts and Windows Forms controls
    programming
    This project is intended for learning and practicing  fundamentals in C#.

---
+++ StudentInfo/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+        private void btnSubmitMessage_Click(object sender, EventArgs e)
+        {
+           if( MessageBox.Show("Are you sure you want to submit this student? ", "Confirm",MessageBoxButtons.OKCancel, MessageBoxIcon.Question ,MessageBoxDefaultButton.Button1) == DialogResult.OK )
+            {
+                MessageBox.Show("User Pressed Ok");
+            }
+           
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Are you sure you want to cancel this student?","Confirm",MessageBoxButtons.OKCancel,MessageBoxIcon.Question,MessageBoxDefaultButton.Button2) == DialogResult.OK)
+            {
+                MessageBox.Show("User Pressed Ok");
+            }
+            Application.Exit();
+        }
+    }
+}
